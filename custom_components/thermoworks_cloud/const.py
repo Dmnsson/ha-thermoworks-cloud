345b1e36b8430bea6c5f00aaea8cdd864e9f629c
lcoppenr <40072517+lcoppenr@users.noreply.github.com>
2026-06-07 14:01:35 -0700

    v2026.6.7.001: merge upstream ETI Cloud provider support + update vendored AuthFactory

--- custom_components/thermoworks_cloud/const.py
+++ custom_components/thermoworks_cloud/const.py
@@ -7,3 +7,23 @@ DEFAULT_SCAN_INTERVAL_SECONDS = 180
 MIN_SCAN_INTERVAL_SECONDS = 60  # 1 minute
 # Active cook detection polling interval
 ACTIVE_COOK_SCAN_INTERVAL_SECONDS = 60  # 1 minute when cook is active
+
+CONF_CLOUD_PROVIDER = "cloud_provider"
+
+PROVIDER_THERMOWORKS = "thermoworks"
+PROVIDER_ETI = "eti"
+
+CLOUD_PROVIDERS = {
+    PROVIDER_THERMOWORKS: {
+        "name": "ThermoWorks Cloud",
+        "api_key": None,  # Use library defaults
+        "app_id": None,
+        "referer": None,
+    },
+    PROVIDER_ETI: {
+        "name": "ETI Cloud",
+        "api_key": "AIzaSyBD4snlT2LllO4k0NywX5qYjJ7M7WfU4_I",
+        "app_id": "1:701566661301:web:7a9bc711c05985ead144fc",
+        "referer": "https://cloud.etiltd.com/",
+    },
+}
